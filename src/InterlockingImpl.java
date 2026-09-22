55fe6e03a47cc7d3db47875ff3199fbe27fabf2b
BigJimmy333 <james.athanasopoulos@yahoo.com.au>
2026-09-22 22:43:56 +0930

    Add a map that tracks the places and trains

--- src/InterlockingImpl.java
+++ src/InterlockingImpl.java
@@ -1,4 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class InterlockingImpl implements Interlocking {
+
+    //Create a value for each place to track train positions
+    private Map<Integer, String> places = new HashMap<>();
+
+    public InterlockingImpl() {
+        //11 because theres 11 places in the petri net
+        for (int i = 1; i <= 11; i++){
+            places.put(i, null);
+        }
+    }
 
     @Override
     public void addTrain(String trainName, int entryTrackSection, int destinationTrackSection) throws IllegalArgumentException, IllegalStateException {
