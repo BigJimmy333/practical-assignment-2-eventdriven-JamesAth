6681384d502e35f6a5f2b4ecf2d6d7299e37f476
BigJimmy333 <james.athanasopoulos@yahoo.com.au>
2026-09-23 13:23:19 +0930

    Test: Add test to check if get train throws error as expected

--- src/InterlockingImpl_Test.java
+++ src/InterlockingImpl_Test.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 
 public class InterlockingImpl_Test {
     
+    // === GET SECTION TESTS ===
+    //Test if a train is in a section, should be null because no train has been added
     @Test
     public void testGetSection(){
         InterlockingImpl interlockingImpl = new  InterlockingImpl();
@@ -12,10 +14,19 @@ public class InterlockingImpl_Test {
         assertNull(trainName);
     }
 
+    //Test if a section 33 exists, it should not because theres only 11 sections
     @Test(expected = IllegalArgumentException.class)
     public void testGetSectionException(){
         InterlockingImpl interlockingImpl = new  InterlockingImpl();
         interlockingImpl.getSection(33);
+    }
+
+    // === GET TRAIN TESTS ===
+    //No trains have been added yet so should throw
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetTrainEmptyList(){
+        InterlockingImpl interlockingImpl = new  InterlockingImpl();
+        interlockingImpl.getTrain("Train 33");
     }
 
 }
